67c49f5044d14864580848a109dec66dae24a66f
supreethavadhani <supreethavadhani@gmail.com>
2023-03-05 19:08:41 -0800

    updated service

--- src/app/services/constantsService.ts
+++ src/app/services/constantsService.ts
@@ -11,7 +11,7 @@ export class ConstantsService {
   public static ROUTE_NON_SESSION = "non-session";
   public static ROUTE_SESSION = "session";
 
-  public static getFormFd(formName:String,sa:ServiceAgent) {
+  public static getFormFd(formName:string,sa:ServiceAgent) {
     let form = allServices[formName].getInstance()
     let fd = new FormData(form,sa)
     return fd
